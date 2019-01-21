dd3c0fbb213ca2830df1998fc94717fad05b7d20
Bob Weichler <bobmasaki@hotmail.com>
2019-01-21 21:13:30 +0100

    Adding user login (basics)

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -4,22 +4,33 @@
 		<input type="text" placeholder="email" v-model="email" />
 		<input type="password" placeholder="***" v-model="password" />
 		<button @click="login">Login</button>
+		<p class="err-txt" v-show="error">{{error}}</p>
 		<p>Don't have an account yet? <router-link to="/register">Create one here</router-link></p>
 	</div>
 </template>
 
 <script>
+	import firebase from 'firebase'
+
 	export default{
 		name: 'login',
 		data(){
 			return{
 				email: '',
-				password: ''
+				password: '',
+				error: null
 			}
 		},
 		methods: {
 			login(){
-				this.$router.replace('home');
+				firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+					.then(user => {
+						console.log(`You're logged in!`);
+					})
+					.catch(err => {
+						this.error = err.message
+					});
+				// this.$router.replace('home');
 			}
 		}
 	}
